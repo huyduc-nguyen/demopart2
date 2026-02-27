50c579cb1253b8eca9020dbc5c5450aa13f3cefa
huyduc-nguyen <duytan0123huy@gmail.com>
2026-02-27 16:22:24 +0700

    bắn súng có âm thanh

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Bắt buộc phải có dòng này
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Bạn phải có chữ 'public' thì Unity mới tìm thấy hàm này
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("trangchu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
